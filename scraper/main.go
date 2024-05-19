dc223c45de1b55a6e019b9b28e14479d430c4321
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2024-05-19 01:11:51 -0500

    update to latest numbers

--- scraper/main.go
+++ scraper/main.go
@@ -22,15 +22,15 @@ func main() {
 
 	client := http.Client{}
 
-	// err = games.Archive(3157, pwd, client, db)
+	// err = games.Archive(3158, pwd, client, db)
 	// err = achievements.Archive(35, pwd, client, db)
-	// err = designs.Archive(5710, pwd, client, db)
+	// err = designs.Archive(5720, pwd, client, db)
 	// err = products.Archive(1826, pwd, client, db)
 	// err = sets.Archive(99, pwd, client, db)
 	// err = clubs.Archive(103, pwd, client, db)
 	// err = club_roles.Archive(468, pwd, client, db)
 	err = club_members.Archive(468, pwd, client, db)
-	// err = users.Archive(11906, pwd, client, db)
+	// err = users.Archive(11907, pwd, client, db)
 
 	if err != nil {
 		panic(err)
