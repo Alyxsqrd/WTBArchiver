c233aa583a5104d63ca7905e68502370b438dbd3
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2024-05-21 00:01:15 -0500

    organization - RIP wtb
    officially shutdown

--- scraper/main.go
+++ scraper/main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"net/http"
 	"os"
-	"scraper/reputation"
 )
 
 func main() {
@@ -22,20 +21,64 @@ func main() {
 
 	client := http.Client{}
 
+	// Game Archival Tools ----------------------
+
 	// err = games.Archive(3158, pwd, client, db)
+
+	// ------------------------------------------
+
+	// Achievement Archival Tools ----------------------
+
 	// err = achievements.Archive(35, pwd, client, db)
-	// err = designs.Archive(5720, pwd, client, db)
-	// err = products.Archive(1826, pwd, client, db)
-	// err = forum_replies.Archive(2856, pwd, client, db)
-	//err = forum_posts.Archive(2856, pwd, client, db)
-	// err = sets.Archive(99, pwd, client, db)
+	// err = user_achievements.Archive(11907, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// Club Archival Tools -----------------------------
+
 	// err = clubs.Archive(103, pwd, client, db)
 	// err = club_roles.Archive(468, pwd, client, db)
-	//err = club_members.Archive(468, pwd, client, db)
+	// err = club_members.Archive(468, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// Sets Archival Tools -----------------------------
+
+	// err = sets.Archive(99, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// Pets Archival Tools -----------------------------
+
 	// err = pets.Archive(26, pwd, client, db)
-	// err = user_achievements.Archive(11907, pwd, client, db)
-	//err = users.Archive(11907, pwd, client, db)
-	err = reputation.Archive(11907, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// User Archival Tools -----------------------------
+
+	// err = users.Archive(11907, pwd, client, db)
+	// err = reputation.Archive(11907, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// Forum Archival Tools -----------------------------
+
+	// err = forum_replies.Archive(2856, pwd, client, db)
+	// err = forum_posts.Archive(2856, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// Design Archival Tools -----------------------------
+
+	// err = designs.Archive(5720, pwd, client, db)
+
+	// -------------------------------------------------
+
+	// Product Archival Tools -----------------------------
+
+	// err = products.Archive(1826, pwd, client, db)
+
+	// -------------------------------------------------
 
 	if err != nil {
 		panic(err)
